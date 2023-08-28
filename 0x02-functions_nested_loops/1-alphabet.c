c9481fc814238d00613a38ea387573af6c3f859f
Stanley3242 <stanleyudegbo2018@gmail.com>
2023-08-28 04:35:54 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - function print alphabet
+ *
+ * Return: always as 0
+ */
+
+void print_alphabet(void);
+
+{
+	 char j;
+
+	for (j = 'a' ; j <= 'z' j++)
+	(
+		_putchar(j);
+	)
+	_putchar('\n');
+}
